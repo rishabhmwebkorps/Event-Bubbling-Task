b290ac9bf91540a9ebb153e463c91be56e68f5a2
Rishabh Mishra <rishabh.m@webkorps.com>
2024-08-21 19:37:05 +0530

    random number fixes

--- script.js
+++ script.js
@@ -1,76 +1,74 @@
 
 
-const circleContainer = document.querySelector('.numbersContainer');
-
 let coins = 0;
 let randomNumber;
-let timeLeft = 60;
 let timerInterval;
 
+    function startGame() {
+          const randomNumberBox = document.getElementById('targetNumber');
+          randomNumberBox.innerHTML = ` Click Number (${(randomNumber = Math.floor(Math.random() * 20) + 1)})`;
+          circlesNumber();
+          timerHeading();
+       }
 
 
-function startGame() {
-    const randomNumberBox = document.getElementById('targetNumber');
-    randomNumberBox.textContent = ` Click Number (${(randomNumber = Math.floor(Math.random() * 20) + 1)})`;
-    circlesNumber();
-    timerHeading();
-}
-startGame();
+        startGame();
 
-function circlesNumber() {
-    const circles = document.querySelectorAll('.circle');
-    const randomCircle = Math.floor(Math.random() * circles.length);
-    console.log(circles.length, "Circle length");
+          function circlesNumber() {
+             const circles = document.querySelectorAll('.circle');
+             const randomCircle = Math.floor(Math.random() * circles.length);
 
-    circles.forEach((circle, index) => {
-        let number;
-        if (index == randomCircle) {
-            number = randomNumber;
-        } else {
-            number = Math.floor(Math.random() * 10) + 1;
-        }
-        circle.textContent = number;
-    });
-}
+            circles.forEach((circle, index) => {
+                let number;
+               if (index == randomCircle) {
+                 number = randomNumber;
+                } else {
+              number = Math.floor(Math.random() * 10) + 1;
+            }
+           circle.innerHTML = number;
+     });
+    }
 
-function timerHeading() {
-    const timerSection = document.getElementById('timer');
-    clearInterval(timerInterval);
-    timePending = 60;
-    timerSection.textContent = timeLeft;
+        function timerHeading() {
+             const timerSection = document.getElementById('timer');
+              clearInterval(timerInterval);
+              let timeLeft = 90;
+               timerSection.innerHTML = timeLeft;
 
-    timerInterval = setInterval(() => {
-        timerSection.textContent = `timer ${--timePending}`;
+          timerInterval = setInterval(() => {
+               timerSection.innerHTML = `timer ${--timeLeft}`;
 
-        if (timePending <= 0) {
+            if (timeLeft <= 0) {
+                clearInterval(timerInterval);
+                  alert(`Time is over ${coins}`);
+                restartGame();
+         }
+         }, 1000);
+       }
+
+        function restartGame() {
+           const coinsBox = document.getElementById('coins');
             clearInterval(timerInterval);
-            alert(`Time is over ${coins}`);
-            restartGame();
-        }
-    }, 1000);
-}
-
-function restartGame() {
-    const coinsBox = document.getElementById('coins');
-    clearInterval(timerInterval);
-    coins = 0;
-    coinsBox.textContent = coins;
-    startGame();
-}
-
-circleContainer.addEventListener('click', (e) => {
-    const coinsBox = document.getElementById('coins');
-    if (e.target.classList.contains('circle')) {
-        const selectNumber = parseInt(e.target.textContent);
-
-        if (selectNumber === randomNumber) {
-            coinsBox.textContent = ++coins;
+            coins = 0;
+            coinsBox.innerHTML = coins;
             startGame();
-        } else {
+}
+
+
+      document.querySelector('.numbersContainer').addEventListener('click', (e) => {
+             const coinsBox = document.getElementById('coins');
+          if (e.target.classList.contains('circle')) {
+        const selectNumber = parseInt(e.target.innerHTML);
+
+               if (selectNumber === randomNumber) {
+                  coinsBox.innerHTML = ++coins;
+                  startGame();
+            } else {
             alert(`Your Game is over (${coins}) coins you win`);
             restartGame();
-        }
-    }
-});
+              }
+          }
+     });
+
 
 
